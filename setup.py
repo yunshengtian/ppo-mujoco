d001e3d5ea6739e4a04cede43706fd271a6c8a9c
Yunsheng Tian <yunsheng@mit.edu>
2020-12-30 15:53:02 -0500

    rename

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='mujoco-ppo',
+    name='ppo-mujoco',
     packages=find_packages(),
     version='0.0.1',
     install_requires=['gym', 'matplotlib', 'pybullet', 'torch', 'mujoco_py'])
