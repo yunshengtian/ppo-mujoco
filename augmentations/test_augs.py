5146f6f10e89519b65a7f8ff7a358ffa98a22eb3
Dylan Yung <dyung6@gatech.edu>
2022-05-10 21:28:00 -0400

    added test augs

--- augmentations/test_augs.py
+++ augmentations/test_augs.py
@@ -1,4 +1,7 @@
 import augmentations.rad as rad
+from os import listdir
+from os.path import isfile, join
 
 if __name__ == "__main__":
-    pass
+    folder = './mujoco_samples/'
+    onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
